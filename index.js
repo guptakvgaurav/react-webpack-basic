97a3f1a1d5fc1e47f9a24c8cc4aae0d1b04868eb
Manoj Nama <manoj.nama@outlook.com>
2017-04-24 12:20:57 +0530

    Adding middlewares, multiple reducers

--- index.js
+++ index.js
@@ -1,3 +1,69 @@
 //https://github.com/manoj-nama/react-webpack-basic
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
-console.log("This is React");
+const reducer = (state = {a: 10, counter: 0}, action) => {
+  switch(action.type) {
+    case 'INC': {
+      return {
+        ...state, counter: state.counter + action.val
+      }
+    }
+    case 'DEC': {
+      return {
+        ...state, counter: state.counter - action.val
+      };
+    }
+    case 'FOO': {
+      return {
+        ...state,
+        a: 'xyz'
+      }
+    }
+  }
+  return state;
+}
+
+const reducerTwo = (state = 0, action) => {
+  switch(action.type) {
+    case 'INC': {
+      return state + action.val;
+    }
+    case 'DEC': {
+      return state - action.val;
+    }
+  }
+  return state;
+}
+
+const logger = (store) => (next) => (action) => {
+  console.log(action.type, store.getState());
+  next(action);
+}
+
+const reducers = combineReducers({
+  state1: reducer,//{a: 10, counter: 0}
+  state2: reducerTwo//0
+});
+
+const middlewares = applyMiddleware(logger);
+
+const store = createStore(reducers, middlewares);
+
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+function increment(val) {
+  return {type: 'INC', val};
+}
+
+store.dispatch(increment(1));
+store.dispatch(increment(2));
+store.dispatch(increment(3));
+store.dispatch(increment(4));
+store.dispatch(increment(5));
+store.dispatch({type: 'DEC', val: 2});
+store.dispatch({type: 'DEC', val: 2});
+store.dispatch(increment(1));
+store.dispatch({type: 'FOO'});
